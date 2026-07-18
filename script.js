af645fabd9a02b2a982d46b4b8e2e846d533283b
bluetale993-spec <bluetale993@gmail.com>
2026-07-18 22:59:47 +0530

    Redirect to welcome.html on gift button click

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-alert("JavaScript is working!");
-
 const giftBtn = document.getElementById("giftBtn");
 
 if (giftBtn) {
-  giftBtn.addEventListener("click", () => {
-    alert("✨ Welcome! Your magical birthday surprise is just getting started... 💖");
-  });
+    giftBtn.addEventListener("click", () => {
+        window.location.href = "welcome.html";
+    });
 }
