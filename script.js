8662cf72bb24c949d25f3358ae44d47ce185ce16
bluetale993-spec <bluetale993@gmail.com>
2026-07-18 22:40:28 +0530

    Add birthday surprise alert on button click

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-// Birthday website JavaScript
+const giftBtn = document.getElementById("giftBtn");
+
+giftBtn.addEventListener("click", () => {
+    alert("✨ Welcome! Your magical birthday surprise is just getting started... 💖");
+});
