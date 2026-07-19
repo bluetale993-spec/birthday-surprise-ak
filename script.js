4c1a53d4f12a08d0486b239237d7aad764ebf74b
bluetale993-spec <bluetale993@gmail.com>
2026-07-19 10:36:42 +0530

    Fix formatting of window location redirect

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const giftBtn = document.getElementById("giftBtn");
 
 if (giftBtn) {
     giftBtn.addEventListener("click", () => {
-        window.location.href = "welcome.html";
+        window.location.href ="welcome.html";
     });
 }
